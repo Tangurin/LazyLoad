baa9d3c1a39f8dcfef01bf6322c6645811c71fa1
Albert <webbhelp_@hotmail.com>
2016-10-06 15:35:20 +0200

    Try replace all lazy load img, src attributes

--- LazyLoad.js
+++ LazyLoad.js
@@ -1,20 +1,20 @@
-var Lazyload = {
+var LazyLoad = {
     elements: [],
     elementLength: 0,
+    offset: 100,
     offsets: [],
     scrollElement: null,
     windowHeight: 0,
     windowInnerHeight: 0,
-    zindex: 0,
     debug: true,
     options: {},
     initialize: function(scrollElement) {
         var $window = $(window);
-        Lazyload.windowHeight = $window.height();
-        Lazyload.windowInnerHeight = $window.innerHeight();
-        Lazyload.scrollElement = scrollElement || $window;
+        LazyLoad.windowHeight = $window.height();
+        LazyLoad.windowInnerHeight = $window.innerHeight();
+        LazyLoad.scrollElement = scrollElement || $window;
 
-        Lazyload.options = {
+        LazyLoad.options = {
             opacity: 1,
             top: 0,
             scale: 1,
@@ -22,29 +22,30 @@ var Lazyload = {
         };
 
         //Let content load before loading elements
-        setTimeout(Lazyload.initializeElements, 400)
+        setTimeout(LazyLoad.initializeElements, 400)
     },
     initializeElements: function() {
-        if (!Lazyload.setElements()) return false;
-        Lazyload.listenForScroll();
+        if (!LazyLoad.setElements()) return false;
+        LazyLoad.listenForScroll();
     },
     setElements: function() {
-        var $elements = $('.animateOnScroll');
+        var $elements = $('img[data-lazyLoad], .lazyLoadWrapper');
         var elementLength = $elements.length;
         if (elementLength > 0) {
-            Lazyload.elementLength = elementLength;
+            LazyLoad.elementLength = elementLength;
             var $element;
             var visible;
             for (var i = 0; i < elementLength; i++) {
                 $element = $($elements[i]);
-                visible = Lazyload.isVisible($element);
+                visible = LazyLoad.isVisible($element);
 
-                if (!visible) {
-                    var topOffset = $element.offset().top;
-                    var elementOffset = $element.data('offset') || 0;
-                    Lazyload.elements[i] = $element;
-                    Lazyload.offsets[i] = topOffset + elementOffset;
+                if (visible) {
+                    LazyLoad.loadElement($element);
+                    continue;
                 }
+
+                LazyLoad.elements[i] = $element;
+                LazyLoad.offsets[i] = $element.offset().top + LazyLoad.offset;
             }
             return true;
         }
@@ -52,37 +53,58 @@ var Lazyload = {
     },
     isVisible: function($element) {
       var rect = $element[0].getBoundingClientRect();
-      var viewHeight = Math.max(Lazyload.windowHeight, Lazyload.windowInnerHeight);
+      var viewHeight = Math.max(LazyLoad.windowHeight, LazyLoad.windowInnerHeight);
       var isVisible = !(rect.bottom < 0 || rect.top - viewHeight >= 0);
       return isVisible;
     },
     listenForScroll: function() {
         ScrollHandler.initialize(AnimateOnScroll.scrollElement);
-        
-        var offsets = Lazyload.offsets;
-        Lazyload.scrollElement.on('scroll', function() {
+        console.log(ScrollHandler);
+        var offsets = LazyLoad.offsets;
+        LazyLoad.scrollElement.on('ScrollHandler-Scroll', function() {
+            console.log('a');
             var $this = $(this);
             var currentScroll = $this.scrollTop();
-            var collision = currentScroll + Lazyload.windowHeight;
+            var collision = currentScroll + LazyLoad.windowHeight;
             var $element;
             var offset = 0;
-            for (i = 0; i < Lazyload.elementLength; i++) {
-                offset = Lazyload.offsets[i];
+            for (i = 0; i < LazyLoad.elementLength; i++) {
+                offset = LazyLoad.offsets[i];
                 if (collision > offset) {
-
-                    delete Lazyload.elements[i];
-                    delete Lazyload.offsets[i];
+                    loadElement(LazyLoad.elements[i]);
+                    delete LazyLoad.elements[i];
+                    delete LazyLoad.offsets[i];
                 }
             }
         });
     },
+    handleElement: function($element) {
+        console.log($element);
+        if ($element.hasClass('lazyLoadWrapper')) {
+            var $images = $('img', $element);
+            $.each($images, function(key) {
+                LazyLoad.loadElement($(this));
+            });
+            return true;
+        }
+
+        LazyLoad.loadElement($element);
+    },
+    loadElement: function($element) {
+        var imageName = $element.data('image') || false;
+        if ($element.prop('tagName') != 'IMG' || imageName == false) {
+            return false;
+        }
+
+        $element.attr('src', imageName);
+    },
     debug: function(str) {
-        if (Lazyload.debug) {
+        if (LazyLoad.debug) {
             console.log(str);
         }
     }
 };
 
 $(function() {
-    Lazyload.initialize($('#slidebarMainContent'));
+    LazyLoad.initialize($('#slidebarMainContent'));
 });
